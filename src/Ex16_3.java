4f86faaae2d0dc39a3b84d97eb942d5e3e076509
seungju lee <seungjulee@seungjuui-MacBookPro.local>
2021-10-11 01:53:27 +0900

    add Ex16_4

--- src/Ex16_3.java
+++ src/Ex16_3.java
@@ -8,23 +8,23 @@ class Ex16_3 {
             URL url = new URL(address);
             URLConnection conn = url.openConnection();
             System.out.println("conn.toString():" + conn);
-            System.out.println("getAllowUserInteraction():"
+            System.out.print("getAllowUserInteraction():"
                     + conn.getAllowUserInteraction());
-            System.out.print("\tgetConnectTimeout():" + conn.getConnectTimeout());
+            System.out.println("\t\t\tgetConnectTimeout():" + conn.getConnectTimeout());
             System.out.println("getContent():" + conn.getContent());
-            System.out.println("getContentEncoding():" + conn.getContentEncoding());
-            System.out.print("\tgetContentLength():" + conn.getContentLength());
-            System.out.println("getContentType():" + conn.getContentType());
-            System.out.print("\tgetDate():" + conn.getDate());
+            System.out.print("getContentEncoding():" + conn.getContentEncoding());
+            System.out.println("\t\t\tgetContentLength():" + conn.getContentLength());
+            System.out.print("getContentType():" + conn.getContentType());
+            System.out.println("\tgetDate():" + conn.getDate());
             System.out.println("getDefaultAllowUserInteraction():"
                     + conn.getDefaultAllowUserInteraction());
-            System.out.println("getDefaultUseCaches():" + conn.getDefaultUseCaches());
-            System.out.print("\tgetDoInput():" + conn.getDoInput());
-            System.out.println("getDoOutput():" + conn.getDoOutput());
-            System.out.println("\tgetExpiration():" + conn.getExpiration());
+            System.out.print("getDefaultUseCaches():" + conn.getDefaultUseCaches());
+            System.out.println("\t\t\tgetDoInput():" + conn.getDoInput());
+            System.out.print("getDoOutput():" + conn.getDoOutput());
+            System.out.println("\t\t\t\tgetExpiration():" + conn.getExpiration());
             System.out.println("getHeaderFields():" + conn.getHeaderFields());
-            System.out.println("getIfModifiedSince():" + conn.getIfModifiedSince());
-            System.out.println("\tgetLastModified():" + conn.getLastModified());
+            System.out.print("getIfModifiedSince():" + conn.getIfModifiedSince());
+            System.out.println("\t\t\t\tgetLastModified():" + conn.getLastModified());
             System.out.println("getReadTimeout():" + conn.getReadTimeout());
             System.out.println("getURL():" + conn.getURL());
             System.out.println("getUseCaches():" + conn.getUseCaches());
